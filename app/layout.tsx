693ed76880e3b634a6209680bfe56f5fcfad3483
Mahziyar Erfani <mhzrerfani@gmail.com>
2025-02-24 14:24:52 +0330

    style(chore): Update font family

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import type React from "react";
-import { Lora } from "next/font/google";
+import { Inter } from "next/font/google";
 import Providers from "@/providers";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 
-const playfair = Lora({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={playfair.className}>
+      <body className={`${inter.className} font-sans`}>
         <Providers>
           {children}
           <Toaster />
